c1cc1dbd8e43f72e75bf2ee8e91c1c8b34e90ec0
Sujit115 <tamangsujit35@gmail.com>
2020-01-20 13:55:10 +0545

    modified listing page

--- realtor/models.py
+++ realtor/models.py
@@ -4,11 +4,11 @@ from datetime import datetime
 
 class Realtor(models.Model):
   
-  name = models.CharField(max_length=200, default = "")
-  photo = models.ImageField(upload_to='photos/%Y/%m/%d/', default = "")
-  description = models.TextField(blank=True, default = "")
-  phone = models.CharField(max_length=20, default = "")
-  email = models.CharField(max_length=50, default = "")
+  name = models.CharField(max_length=200)
+  photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+  description = models.TextField(blank=True)
+  phone = models.CharField(max_length=20)
+  email = models.CharField(max_length=50)
   is_mvp = models.BooleanField(default=False)
   hire_date = models.DateTimeField(default=datetime.now, blank=True)
 
